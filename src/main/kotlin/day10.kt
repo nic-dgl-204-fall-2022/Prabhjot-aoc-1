5e10735c3891d64dda04799900a5bd3a209cd813
Prabhjot <61209227+08prabhjot@users.noreply.github.com>
2022-10-18 02:06:10 +0530

    Updated final solution code

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -18,7 +18,13 @@ fun main(){
         return result.toString()
     }
 
-    val answer = lookAndSay(9644489995, 40)
+    println("Hello, we are playing here with look and say numbers. The problem of Day-10 in AOC-2015 ask us to print the length of look & say numbers applied till 40 (part 1) or 50 (part 2) times. So, please enter a number-")
+     val sampleNumber = readLine()!!.toString()
+
+    println("How many times 40 or 50?")
+    val timesNumber = readLine()!!.toInt()
+
+    val answer = lookAndSay(sampleNumber, timesNumber)
     println(answer)
 
 }
